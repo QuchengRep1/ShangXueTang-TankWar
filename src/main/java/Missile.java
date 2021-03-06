8640a762ed9a5c16f2f7367aadba631ff65e290a
QuchengRep1 <QuchengRep_1@163.com>
2021-03-06 16:49:42 +0800

    增加子弹类，实现了子弹的飞行同步坦克的行动方向。 待处理项目： 1、子弹的位置调整至主站坦克的正中位置； 2、子弹在坦克停止状态下也能完成发射，而非取STOP状态。

--- src/main/java/Missile.java
+++ src/main/java/Missile.java
@@ -5,6 +5,9 @@ public class Missile {
     int x, y;
     private static final int XSPEED = 10;
     private static final int YSPEED = 10;
+    public static final int WIDTH = 10;
+    public static final int HEIGHT = 10;
+
     Tank.Direction dir;
 
     public Missile(int x, int y, Tank.Direction dir) {
@@ -16,7 +19,7 @@ public class Missile {
     public void draw(Graphics g) {
         Color c =  g.getColor();
         g.setColor(Color.BLACK);
-        g.fillOval(x,y,10,10);
+        g.fillOval(x,y,WIDTH,HEIGHT);
         g.setColor(c);
         move();
 
